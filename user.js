d9ab1d65f712b9e2b1e9ed9f772b8f9c79af49db
Yùjǐn Lín <yjlin0224@outlook.com>
2026-04-04 22:41:04 +0800

    Update to latest

--- user.js
+++ user.js
@@ -32,17 +32,18 @@ user_pref("devtools.screenshot.audio.enabled", false);
 user_pref("devtools.toolbox.host", "right");
 user_pref("extensions.activeThemeID", "default-theme@mozilla.org");
 user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.update.enabled", true);
 user_pref("findbar.highlightAll", true);
 user_pref("font.default.x-unicode", "sans-serif");
 user_pref("font.default.x-western", "sans-serif");
 user_pref("font.language.group", "zh-TW");
-user_pref("font.name.monospace.ja", "Fira Code");
-user_pref("font.name.monospace.ko", "Fira Code");
-user_pref("font.name.monospace.x-unicode", "Fira Code");
-user_pref("font.name.monospace.x-western", "Fira Code");
-user_pref("font.name.monospace.zh-CN", "Fira Code");
-user_pref("font.name.monospace.zh-HK", "Fira Code");
-user_pref("font.name.monospace.zh-TW", "Fira Code");
+user_pref("font.name.monospace.ja", "Maple Mono NF CN");
+user_pref("font.name.monospace.ko", "Maple Mono NF CN");
+user_pref("font.name.monospace.x-unicode", "Maple Mono NF CN");
+user_pref("font.name.monospace.x-western", "Maple Mono NF CN");
+user_pref("font.name.monospace.zh-CN", "Maple Mono NF CN");
+user_pref("font.name.monospace.zh-HK", "Maple Mono NF CN");
+user_pref("font.name.monospace.zh-TW", "Maple Mono NF CN");
 user_pref("font.name.sans-serif.ja", "Noto Sans CJK JP");
 user_pref("font.name.sans-serif.ko", "Noto Sans CJK KR");
 user_pref("font.name.sans-serif.x-unicode", "Noto Sans");
@@ -76,26 +77,19 @@ user_pref("places.history.expiration.max_pages", 2147483647);
 user_pref("places.history.expiration.transient_current_max_pages", 2147483647);
 user_pref("privacy.donottrackheader.enabled", true);
 user_pref("privacy.fingerprintingProtection", true);
-user_pref(
-  "privacy.fingerprintingProtection.overrides",
-  "+AllTargets,-CSSPrefersColorScheme,-CanvasExtractionBeforeUserInputIsBlocked,-CanvasExtractionFromThirdPartiesIsBlocked,-CanvasImageExtractionPrompt,-CanvasRandomization,-FontVisibilityBaseSystem,-FontVisibilityLangPack,-JSDateTimeUTC,-KeyboardEvents,-WidgetEvents,-WindowDevicePixelRatio",
-);
+user_pref("privacy.fingerprintingProtection.overrides", "+AllTargets,-CSSPrefersColorScheme,-CanvasExtractionBeforeUserInputIsBlocked,-CanvasExtractionFromThirdPartiesIsBlocked,-CanvasImageExtractionPrompt,-CanvasRandomization,-FontVisibilityBaseSystem,-FontVisibilityLangPack,-JSDateTimeUTC,-KeyboardEvents,-WidgetEvents,-WindowDevicePixelRatio");
 user_pref("privacy.query_stripping.enabled", true);
 user_pref("privacy.query_stripping.enabled.pbmode", true);
-user_pref(
-  "privacy.query_stripping.strip_list",
-  "__hsfp __hssc __hstc __s _hsenc _openstat dclid fbclid gbraid gclid hsCtaTracking igshid mc_eid ml_subscriber ml_subscriber_hash msclkid oft_c oft_ck oft_d oft_id oft_ids oft_k oft_lk oft_sk oly_anon_id oly_enc_id rb_clickid s_cid twclid vero_conv vero_id wbraid wickedid yclid  __hsfp __hssc __hstc __s _gl _hsenc _kx _openstat at_recipient_id at_recipient_list bbeml bsft_clkid bsft_uid dclid et_rid fb_action_ids fb_comment_id fbclid gbraid gclid guce_referrer guce_referrer_sig hsCtaTracking irclickid mc_eid ml_subscriber ml_subscriber_hash msclkid mtm_cid oft_c oft_ck oft_d oft_id oft_ids oft_k oft_lk oft_sk oly_anon_id oly_enc_id pk_cid rb_clickid s_cid ss_email_id twclid unicorn_click_id vero_conv vero_id vgo_ee wbraid wickedid yclid ymclid ysclid h_sid h_slt mkt_tok igsh igshid ref_src ref_url si __cft__[0] __tn__",
-); // https://github.com/brave/brave-core/blob/master/components/query_filter/utils.cc
+user_pref("privacy.query_stripping.strip_list", "__hsfp __hssc __hstc __s _hsenc _openstat dclid fbclid gbraid gclid hsCtaTracking igshid mc_eid ml_subscriber ml_subscriber_hash msclkid oft_c oft_ck oft_d oft_id oft_ids oft_k oft_lk oft_sk oly_anon_id oly_enc_id rb_clickid s_cid twclid vero_conv vero_id wbraid wickedid yclid  __hsfp __hssc __hstc __s _gl _hsenc _kx _openstat at_recipient_id at_recipient_list bbeml bsft_clkid bsft_uid dclid et_rid fb_action_ids fb_comment_id fbclid gbraid gclid guce_referrer guce_referrer_sig hsCtaTracking irclickid mc_eid ml_subscriber ml_subscriber_hash msclkid mtm_cid oft_c oft_ck oft_d oft_id oft_ids oft_k oft_lk oft_sk oly_anon_id oly_enc_id pk_cid rb_clickid s_cid ss_email_id twclid unicorn_click_id vero_conv vero_id vgo_ee wbraid wickedid yclid ymclid ysclid h_sid h_slt mkt_tok igsh igshid ref_src ref_url si __cft__[0] __tn__"); // https://github.com/brave/brave-core/blob/master/components/query_filter/utils.cc
 user_pref("privacy.trackingprotection.socialtracking.enabled", true);
 user_pref("privacy.userContext.enabled", true);
 user_pref("privacy.userContext.ui.enabled", true);
 user_pref("svg.context-properties.content.enabled", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref(
-  "toolkit.zoomManager.zoomValues",
-  "0.1,0.125,0.2,0.25,0.3,0.3333333333333333,0.375,0.4,0.5,0.6,0.625,0.6666666666666666,0.7,0.75,0.8,0.875,0.9,1.0,1.25,1.3333333333333333,1.5,1.6666666666666667,1.75,2.0,2.5,3.0,3.5,4.0",
-);
+user_pref("toolkit.zoomManager.zoomValues", "0.1,0.125,0.2,0.25,0.3,0.3333333333333333,0.375,0.4,0.5,0.6,0.625,0.6666666666666666,0.7,0.75,0.8,0.875,0.9,1.0,1.25,1.3333333333333333,1.5,1.6666666666666667,1.75,2.0,2.5,3.0,3.5,4.0");
 user_pref("widget.windows.mica", true);
+user_pref("widget.windows.mica.popups", 1);
+user_pref("widget.windows.mica.toplevel-backdrop", 1);
 user_pref("browser.theme.native-theme", true);
 user_pref("gfx.webrender.compositor.force-enabled", true);
 user_pref("sidebar.revamp", true);
